f779ed885e861e3c1ed7c7a6000124969575ea1d
dtown <devonstownsend@gmail.com>
2014-11-21 15:47:35 +1000

    backend and api stuff

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var port = process.env.PORT || 3000;
 
 app.set('port', port);
 
-require('./routes/routes.js')(app); // pass in app and passport
-require('./routes/api/api.js')(app);
-
 app.use(express.static(__dirname + '/public'));
 
-// app.get('/', function(request, response) {
-// response.send('<h1>Hey whats up! your username is '+randomUsername()+'! :)</h1>');
-// })
+app.get('/', function(req, res) {
+	// res.render('index');
+	res.send('HIIIIIIIIIII whats up.  Cody, replace this with rendering the front end.');
+});
+
+require('./routes/api/api.js')(app);
 
 app.listen(port, function() {
  console.log('Server listening at port %d', port);
@@ -23,29 +23,3 @@ app.use(function(req, res, next) {
  err.status = 404;
  next(err);
 });
-
-
-// var express = require('express')
-// var app = express();
-
-// app.set('port', (process.env.PORT || 3000))
-// app.use(express.static(__dirname + '/public'))
-
-// app.get('/', function(request, response) {
-//   response.send('<h1>Hey whats up! your username is '+randomUsername()+'! :)</h1>');
-// })
-
-// app.listen(app.get('port'), function() {
-//   console.log("Node app is running at localhost:" + app.get('port'))
-// })
-
-// function randomUsername() {
-//   var nouns = ['fart','weed','poop','snowboard','longboarding','blaze','pussy','meat','slippery','dumb','heady','messy','drunk','blood','unintended','uncool', 'fartridden'];
-//   var descriptors = ['fan','dude','man','doctor','expert','thug','hero','king','queen','idiot','queef','muscles','splatter','satan','worshipper', 'virgin', 'boy', 'girl','badbitch'];
-//   var numbers = ['420','69'];
-//   var noun = nouns[Math.floor(Math.random() * nouns.length)];
-//   var descriptor = descriptors[Math.floor(Math.random() * descriptors.length)];
-//   var number = numbers[Math.floor(Math.random() * numbers.length)];
-//   return noun+descriptor+number;
-// }
-
