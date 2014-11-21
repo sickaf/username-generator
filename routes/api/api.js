e9a61b6e11abccb1689c905d376b3bff751aa0b5
dtown <devonstownsend@gmail.com>
2014-11-21 16:01:57 +1000

    now the api capitalizes the names

--- routes/api/api.js
+++ routes/api/api.js
@@ -11,7 +11,9 @@ module.exports = function(app) {
   		var descriptors = ['fan','dude','man','doctor','expert','thug','hero','king','queen','idiot','queef','muscles','splatter','satan','worshipper', 'virgin', 'boy', 'girl','badbitch'];
   		var numbers = ['420','69'];
   		var noun = nouns[Math.floor(Math.random() * nouns.length)];
+  		noun = noun.charAt(0).toUpperCase() + noun.substring(1);
   		var descriptor = descriptors[Math.floor(Math.random() * descriptors.length)];
+  		descriptor = descriptor.charAt(0).toUpperCase() + descriptor.substring(1);
   		var number = numbers[Math.floor(Math.random() * numbers.length)];
   		return noun+descriptor+number;
 	}
