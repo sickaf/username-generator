3eb9ab06df7756af7fbaa35f555991022e1a5fcd
dtown <devonstownsend@gmail.com>
2014-11-23 21:54:39 +1100

    more words

--- routes/api/api.js
+++ routes/api/api.js
@@ -8,27 +8,28 @@ module.exports = function(app) {
 
 	function randomUsername() {
   		
-  		var nouns = [	'fart',     'weed',       'poop',       'snowboard',    'longboarding', 
-  						'blaze',    'pussy',      'meat',       'slippery',     'dumb', 
-  						'heady',    'messy',      'drunk',      'blood',        'unintended', 
-  						'uncool',   'fartridden', 'decrepit',   'justbad',      'impressively',
-  						'gross',    'pathetic',   'floppy',     'chrisbrown',   'hurt',
-  						'DJ',       'fragile',    'used',       'abused',       'shit',
-  						'fuck',     'gushy',      'mushy',      'blast',        'soggy',
-  						'frothy',   'awkward',    'shredded',   'piss',         'puss',
-  						'ass',      'terrible',   'sadistic',   'lackluster',   'unfortunate',
-  						'ebola',    'aids',       'massive',    'herpes',       'gonorrhea',
-  						'diarrhea', 'chlamidia',  'aborted',    'fecal',        'doesntpickuponsocialcues',
-  						'heavyset', 'anorexic',   'bulimic',    'unlucky',      'lucky',
-  						'horrible', 'voluptuous', 'punctual',   'outofshape',   'porous',
-  						'poor',     'divorced',   'athletic',   'unathletic',   'mediocre',
-  						'failed',	'precocious', 'prodigious', 'unremarkable', 'spoiled',
-  						'rotten',   'fresh',      'overdue',    'underaccomplished', 'bionic',
-  						'spent',    'destined',   'underwhelming', 'oozing',      'festering',
-  						'pussing',  'tight',      'splitopen',  'depressing',    'depressed',
+  		var nouns = [	'fart',      'weed',       'poop',       'snowboard',    'longboarding', 
+  						'blaze',     'pussy',      'meat',       'slippery',     'dumb', 
+  						'heady',     'messy',      'drunk',      'blood',        'unintended', 
+  						'uncool',    'fartridden', 'decrepit',   'justbad',      'impressively',
+  						'gross',     'pathetic',   'floppy',     'chrisbrown',   'hurt',
+  						'DJ',        'fragile',    'used',       'abused',       'shit',
+  						'fuck',      'gushy',      'mushy',      'blast',        'soggy',
+  						'frothy',    'awkward',    'shredded',   'piss',         'puss',
+  						'ass',       'terrible',   'sadistic',   'lackluster',   'unfortunate',
+  						'ebola',     'aids',       'massive',    'herpes',       'gonorrhea',
+  						'diarrhea',  'chlamidia',  'aborted',    'fecal',        'doesntpickuponsocialcues',
+  						'heavyset',  'anorexic',   'bulimic',    'unlucky',      'lucky',
+  						'horrible',  'voluptuous', 'punctual',   'outofshape',   'porous',
+  						'poor',      'divorced',   'athletic',   'unathletic',   'mediocre',
+  						'failed',	 'precocious', 'prodigious', 'unremarkable', 'spoiled',
+  						'rotten',    'fresh',      'overdue',    'underaccomplished', 'bionic',
+  						'spent',     'destined',   'underwhelming', 'oozing',      'festering',
+  						'pussing',   'tight',      'splitopen',  'depressing',    'depressed',
   						'overrated', 'meager',    'homely',     'shitty',        'shit',
   						'fucky',     'fuck',      'misused',    'abused',        'recycled',
-  						''   ];
+  						'bleeding',  'bloody',    'guttural',   'blasted',       'cute',
+  						'shameless', 'slaughered', 'unapologetic', ''  ];
   		
   		var descriptors = [	'fan',        'dude',           'man',       'doctor',   'expert', 
   							'thug',       'hero',           'king',      'queen',    'idiot',
@@ -41,17 +42,21 @@ module.exports = function(app) {
   							'diarrhea',   'chlamidia',      'abortion',  'fetus',    'athlete',
   							'failure',    'underwhelmer',   'homie',     'shit',     'fuck',
   							'fuckboy',    'fuckgirl',       'fuckperson', 'fiend',   'dominator',
-  							'cockboy',    'frothball' ];
+  							'cockboy',    'frothball',      'weiner',    'thunderlips', 'meatdrapes',
+  							'thunderlips', 'beefcurtains',  'kumquot',   'obama',     'republican',
+  							'filler'     ];
 
-  		var numbers = ['420','69',''];
-
-      var numbers = ['420','69',''];
+  		var numbers = ['420','69','xXx', 'XOXO', '2014', '14','2000', '', ''];
 
   		var noun = nouns[Math.floor(Math.random() * nouns.length)];
   		noun = noun.charAt(0).toUpperCase() + noun.substring(1);
   		var descriptor = descriptors[Math.floor(Math.random() * descriptors.length)];
   		descriptor = descriptor.charAt(0).toUpperCase() + descriptor.substring(1);
   		var number = numbers[Math.floor(Math.random() * numbers.length)];
+  		var res = noun+descriptor+number;
+
+  		if(!(res.length > 4)) return randomUsername();
+
   		return noun+descriptor+number;
 	}
 
